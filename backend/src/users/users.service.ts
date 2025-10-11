009f7a1f9ab5c907913aa8df0a22177e25c3ce1a
„Christopher <christopher.steins@proton.me>
2025-10-11 02:46:23 +0200

    Implement password hashing with bcrypt for user security

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -23,11 +24,21 @@ export class UsersService {
   }
 
   async create(userData: Partial<User>): Promise<User> {
+    // Hash das Passwort vor dem Speichern
+    if (userData.password) {
+      const saltRounds = 10;
+      userData.password = await bcrypt.hash(userData.password, saltRounds);
+    }
+
     const user = this.usersRepository.create(userData);
     return this.usersRepository.save(user);
   }
 
   async remove(id: string): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async validatePassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword);
   }
 }
