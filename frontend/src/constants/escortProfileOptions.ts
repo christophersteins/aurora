17d419dee805e828fb9fec415defb3133c66b152
„Christopher <christopher.steins@proton.me>
2025-10-15 23:19:35 +0200

    feat: Add dropdown options constants for escort profile

---
+++ frontend/src/constants/escortProfileOptions.ts
@@ -0,0 +1,97 @@
+export const NATIONALITIES = [
+  'Deutsch',
+  'Österreichisch',
+  'Schweizerisch',
+  'Italienisch',
+  'Französisch',
+  'Spanisch',
+  'Polnisch',
+  'Russisch',
+  'Türkisch',
+  'Griechisch',
+  'Rumänisch',
+  'Bulgarisch',
+  'Ungarisch',
+  'Tschechisch',
+  'Amerikanisch',
+  'Britisch',
+  'Brasilianisch',
+  'Kolumbianisch',
+  'Argentinisch',
+  'Chinesisch',
+  'Japanisch',
+  'Thailändisch',
+  'Andere',
+];
+
+export const LANGUAGES = [
+  'Deutsch',
+  'Englisch',
+  'Französisch',
+  'Spanisch',
+  'Italienisch',
+  'Polnisch',
+  'Russisch',
+  'Türkisch',
+  'Griechisch',
+  'Portugiesisch',
+  'Niederländisch',
+  'Arabisch',
+  'Chinesisch',
+  'Japanisch',
+  'Thailändisch',
+];
+
+// Größe in cm (140-220)
+export const HEIGHTS = Array.from({ length: 81 }, (_, i) => 140 + i);
+
+// Gewicht in kg (40-200)
+export const WEIGHTS = Array.from({ length: 161 }, (_, i) => 40 + i);
+
+export const BODY_TYPES = [
+  'Schlank',
+  'Athletisch',
+  'Normal',
+  'Kurvig',
+  'Mollig',
+  'Plus Size',
+];
+
+export const CUP_SIZES = [
+  'AA',
+  'A',
+  'B',
+  'C',
+  'D',
+  'DD',
+  'E',
+  'F',
+  'G',
+  'H',
+];
+
+export const HAIR_COLORS = [
+  'Blond',
+  'Braun',
+  'Schwarz',
+  'Rot',
+  'Grau',
+  'Weiß',
+  'Gefärbt',
+];
+
+export const HAIR_LENGTHS = [
+  'Kurz',
+  'Schulterlang',
+  'Lang',
+  'Sehr lang',
+];
+
+export const EYE_COLORS = [
+  'Blau',
+  'Grün',
+  'Braun',
+  'Grau',
+  'Haselnuss',
+  'Bernstein',
+];
